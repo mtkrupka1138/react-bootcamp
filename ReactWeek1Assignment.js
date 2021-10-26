9308cd60ab8610ddc6397b40d454cfab973790b9
Meghan Krupka <mkrupka.hdm@gmail.com>
2021-10-25 21:44:17 -0400

    Week 1 Assignment initialization

---
+++ ReactWeek1Assignment.js
@@ -0,0 +1,17 @@
+class Student {
+    constructor(name, email, community) {
+        this.name = name;
+        this.email = email;
+        this.community = community;
+    }
+}
+class Bootcamp {
+    constructor(name, level, students=[]) {
+        this.name = name;
+        this.level = level;
+        this.students = students;
+    }
+    registerStudent(studentToRegister) {
+        return students; 
+    }
+}
