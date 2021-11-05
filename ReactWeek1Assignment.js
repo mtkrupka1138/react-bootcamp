354c8e38ad563efebc8a4bd41b01ccb8ecff8231
Meghan Krupka <mkrupka.hdm@gmail.com>
2021-11-05 15:01:01 -0400

    Week 1 assignment Task 2

--- ReactWeek1Assignment.js
+++ ReactWeek1Assignment.js
@@ -5,6 +5,7 @@ class Student {
         this.community = community;
     }
 }
+
 class Bootcamp {
     constructor(name, level, students=[]) {
         this.name = name;
@@ -12,6 +13,14 @@ class Bootcamp {
         this.students = students;
     }
     registerStudent(studentToRegister) {
-        return students; 
+        let checkStudents = this.students.filter(student => student.email === studentToRegister.email)
+        if (checkStudents.length === 0) {
+            this.students.push(studentToRegister);
+            console.log(`Registering ${studentToRegister.name} to the Web Development Bootcamp.`);
+            return this.students;
+        }
+        else {console.log(`${studentToRegister.name} is already registered for this bootcamp!`)}
     }
 }
+
+
