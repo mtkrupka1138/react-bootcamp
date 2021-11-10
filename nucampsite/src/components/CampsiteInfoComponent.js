e03336765c77366d8d120e1fa45b8e48c05ab3f0
Meghan Krupka <mkrupka.hdm@gmail.com>
2021-11-09 20:10:58 -0500

    Week 2 Assiment - Task 2 + render fix

--- nucampsite/src/components/CampsiteInfoComponent.js
+++ nucampsite/src/components/CampsiteInfoComponent.js
@@ -1,27 +1,40 @@
 import React, { Component } from 'react';
+import { Card, CardImg, CardText, CardBody, CardTitle } from 'reactstrap';
 
 class CampsiteInfo extends Component {
     constructor(props) {
         super(props);
         this.state = {
-            
+            selectedCampsite: null
         };
     }
 
-        render(campsite) {
-            if (campsite) {
-                return (
-                    <div className="row"></div>
-                );
-            }
-            return <div></div>;
-        };
+    renderCampsite(campsite) {
+        return (
+            <div className="col-md-5 m-1">
+                <Card>
+                    <CardImg top src={campsite.image} alt={campsite.name} />
+                    <CardBody>
+                        <CardTitle>{campsite.name}</CardTitle>
+                        <CardText>{campsite.description}</CardText>
+                    </CardBody>
+                </Card>
+            </div>
+        );
+    }
 
-
-        renderCampsite() {
-
+    render() {
+        if (this.props.campsite) {
+            return (
+                <div className="row">
+                    {this.renderCampsite(this.props.campsite)}
+                </div>
+            );
         }
-        renderComments() {
+        return <div></div>;
+    };
+    
+    renderComments() {
 
         }
 };
